0eb94bb91b473a342b0ae894a64f473ed55f9b03
HarukaKajita <kajitaj63b3@gmail.com>
2025-07-08 04:36:43 +0900

    Unityからmd-to-pdfのバッチを実行しても出力結果を確認可能に修正

--- Assets/ExtEditor/Editor/BoneOverlay/doc/ConvertMDtoPDF.bat
+++ Assets/ExtEditor/Editor/BoneOverlay/doc/ConvertMDtoPDF.bat
@@ -1,11 +1,18 @@
-rem --- launch self in new console once ---
-if "%~1"=="" (
-    start "" cmd /k "%~f0" _inner
-    goto :eof
+@echo off
+
+rem ---------- Rem/||()でコメントアウトしてる。中身が実行されていない。----------
+rem ---------- Rem/||(と)自体をコメントアウトすると、Unityから実行しても独立したコンソールで実行できるので、終了時に閉じられなくなる。----------
+rem ---------- 出力結果を読めるようになるのでデバッグ目的に使用する。----------
+Rem/||(
+    rem ---------- launch self in new console once ---
+    if "%~1"=="" (
+        start "" cmd /k "%~f0" _inner
+        goto :eof
+    )
 )
 rem ---------- normal path below (arg = _inner) ----------
 
-@echo off
+
 REM Convert all Markdown files in the same directory as this script to PDF using md-to-pdf
 REM Requires: md-to-pdf (https://www.npmjs.com/package/md-to-pdf) installed and available in PATH
 
@@ -36,6 +43,6 @@ popd
 ENDLOCAL
 
 REM Pause so the user can read the output if double-clicked
-echo.
-echo Script finished. Press any key to close this window.
-pause
+rem echo.
+rem echo Script finished. Press any key to close this window.
+rem pause
